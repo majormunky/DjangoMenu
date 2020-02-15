88440472b0550de97176df7a0bf03a06ce49a407
Josh Bright <josh.bright@gmail.com>
2020-02-15 11:32:38 -0800

    Changed the parent field to cascade when we delete stuff

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=64)
     content = models.TextField()
     parent = models.ForeignKey(
-        "self", related_name="children", on_delete=models.PROTECT, null=True, blank=True
+        "self", related_name="children", on_delete=models.CASCADE, null=True, blank=True
     )
 
     def __str__(self):
