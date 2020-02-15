beb8a5ba32a888fa2506a208a191f91638427bc2
Josh Bright <josh.bright@gmail.com>
2020-02-15 10:44:40 -0800

    Added simple way to render menu items, helps to know the depth

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from home import models
 
 
 def index(request):
-    return render(request, "home/index.html", {})
+    menu_items = models.MenuItem.objects.filter(parent__isnull=True)
+    return render(request, "home/index.html", {"menu_items": menu_items})
