755a060d6ec767012f26de67ab1f5af0ab4632e2
Josh Bright <josh.bright@gmail.com>
2020-02-15 10:33:10 -0800

    Added on_delete parameter to menu model parent field

--- home/models.py
+++ home/models.py
@@ -4,4 +4,6 @@ from django.db import models
 class MenuItem(models.Model):
     name = models.CharField(max_length=64)
     content = models.TextField()
-    parent = models.ForeignKey("self", related_name="children")
+    parent = models.ForeignKey(
+        "self", related_name="children", on_delete=models.PROTECT
+    )
