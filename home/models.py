003c34807afbe0d5749daedc05e5cbaec95e4f9a
Josh Bright <josh.bright@gmail.com>
2020-02-15 10:35:06 -0800

    Updated model to allow parent to be null

--- home/models.py
+++ home/models.py
@@ -5,5 +5,5 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=64)
     content = models.TextField()
     parent = models.ForeignKey(
-        "self", related_name="children", on_delete=models.PROTECT
+        "self", related_name="children", on_delete=models.PROTECT, null=True, blank=True
     )
